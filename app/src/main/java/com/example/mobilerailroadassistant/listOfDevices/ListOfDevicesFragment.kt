27559e82708a20dc633c148e5ff365dcf06ae86d
sanahhhh <sasha.sasha.sasha199@mail.ru>
2023-06-18 12:53:47 +0400

    add MapKit

--- app/src/main/java/com/example/mobilerailroadassistant/listOfDevices/ListOfDevicesFragment.kt
+++ app/src/main/java/com/example/mobilerailroadassistant/listOfDevices/ListOfDevicesFragment.kt
@@ -1,22 +1,14 @@
 package com.example.mobilerailroadassistant.listOfDevices
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.SearchView
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import com.example.mobilerailroadassistant.MainActivity
-import com.example.mobilerailroadassistant.R
 import com.example.mobilerailroadassistant.databinding.FragmentListOfDevicesBinding
-import com.example.mobilerailroadassistant.databinding.FragmentRailChainStepOneBinding
-import com.example.mobilerailroadassistant.loggin.LogginFragment
-import com.example.mobilerailroadassistant.railChain.RailChainStepOneFragment
 
 class ListOfDevicesFragment: Fragment() {
 
@@ -25,7 +17,7 @@ class ListOfDevicesFragment: Fragment() {
         get() = _binding ?: throw RuntimeException("FragmentListOfDevicesBinding == null")
     private val viewModel by viewModels<ListOfDevicesViewModel>()
     private val adapterListOfDevices = ListOfDevicesAdapter()
-    private lateinit var searchView: SearchView
+
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
--- app/src/main/java/com/example/mobilerailroadassistant/listOfDevices/ListOfDevicesViewModel.kt
+++ app/src/main/java/com/example/mobilerailroadassistant/listOfDevices/ListOfDevicesViewModel.kt
@@ -3,9 +3,7 @@ package com.example.mobilerailroadassistant.listOfDevices
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.mobilerailroadassistant.LogInAppFragment
 import com.example.mobilerailroadassistant.arrows.ArrowsStepOneFragment
-import com.example.mobilerailroadassistant.loggin.LogginFragment
 import com.example.mobilerailroadassistant.railChain.RailChainStepOneFragment
 
 class ListOfDevicesViewModel : ViewModel() {
